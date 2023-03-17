783c5822a1f6580fddc53a10de02a62d4cbf587b
Tim <timothy.yuen11@gmail.com>
2023-03-17 18:07:20 -0400

    Connected frontend to backend and commented out modal code

--- backend/seeker/src/main/java/com/seeker/seeker/SeekerUser/SeekerUserController.java
+++ backend/seeker/src/main/java/com/seeker/seeker/SeekerUser/SeekerUserController.java
@@ -2,26 +2,26 @@ package com.seeker.seeker.SeekerUser;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 import javax.validation.Valid;
 
 
 @RestController
 @RequestMapping(path = "api/v1/users")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class SeekerUserController {
     
     @Autowired
     private SeekerUserService userService;
 
-    @GetMapping
-    public List<SeekerUser> getAllUsers() {
-        return userService.getAllUsers();
-    }
+    // @GetMapping
+    // public List<SeekerUser> getAllUsers() {
+    //     return userService.getAllUsers();
+    // }
 
     @PostMapping
-    public SeekerUser getUser(@Valid @RequestBody Long id) {
-        return userService.getUser(id);
+    public SeekerUser getUser(@Valid @RequestBody SeekerUser id) {
+        return userService.getUser(id.getId());
     }
 
     @PostMapping(value="/add")
